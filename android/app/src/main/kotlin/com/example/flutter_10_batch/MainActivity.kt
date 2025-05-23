fa9b0fdc0a6d8648ad3b6980d19e4ed87b9d4d55
Hridoy Komol Souman <hridoykomolsouman@gmail.com>
2025-05-23 14:05:38 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_10_batch/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_10_batch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_10_batch/statefull_class.dart';
+import 'flutter_class_1.dart';
+import 'flutter_class_2.dart';
+import 'package:flutter_10_batch/from_login.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'module 9 class 3.dart';
+import 'module_9_class_2.dart';
+import 'module_9_mediaQuery.dart';
+import 'module_9_todo.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'First Method',
+          // You can use the library anywhere in the app even in theme
+          home: child,
+        );
+      },
+      child: const Module9Class3(),
+    );
+  }
+
+}
