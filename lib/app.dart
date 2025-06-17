74644153f461e8e0639ec0422c6d977a8dabed9b
Hridoy Komol Souman <hridoykomolsouman@gmail.com>
2025-06-17 20:58:33 +0600

    module_13

--- lib/app.dart
+++ lib/app.dart
@@ -14,6 +14,7 @@ import 'module 9 class 3.dart';
 import 'module_12/class_1.dart';
 import 'module_12/class_2.dart';
 import 'module_12/class_3.dart';
+import 'module_13/widget/module13.dart';
 import 'module_9_class_2.dart';
 import 'module_9_mediaQuery.dart';
 import 'module_9_todo.dart';
@@ -35,7 +36,7 @@ class MyApp extends StatelessWidget {
           home: child,
         );
       },
-      child: const Module12Class3 (),
+      child: const Module13(),
     );
   }
 
